7b551a9cf6f073521d8ce16783f4b708a869e7cb
Rayanasaad <rayanasaad122@gmail.com>
2024-06-25 22:39:12 +0200

    old debuged code removed

--- tip calculator day 2.py
+++
@@ -1,7 +0,0 @@
-bill = input("Cost")
-tip = input("hom much do you want to tip")
-split = input("how many ways are you spliting the bill")
-tip_in_procent = 1 +int(tip)/100
-your_part =int(bill) / int(split) * tip_in_procent
-your_part1 = round(your_part , 3)
-print (your_part1)
